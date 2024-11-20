7810efdde34a425e13335cfe86287046e92cbd0d
corbin <corbinchristopher1@gmail.com>
2024-11-20 12:33:32 -0700

    continue adding instruction to asm.c

--- asm.c
+++ asm.c
@@ -4,9 +4,30 @@ static const struct Op INSTRUCTIONS[] {
 	[I_MV] = {
 		.name = "MV",
 		.num_args = 2,
-		.args = { , },
+		.args = { REG , IMM8_REG },
 		.macro = false,
 		.instruction = I_MV
+	},
+	[I_LV] = {
+		.name = "LV",
+		.num_args = 2,
+		.args = { REG , IMM16_HL },
+		.macro = false,
+		.instruction = I_LV
+	},
+	[I_SV] = {
+		.name = "SV",
+		.num_args = 2,
+		.args = { IMM18_HL , REG },
+		.macro = false,
+		.instruction = I_SV
+	},
+	[I_PUSH] = {
+		.name = "Push",
+		.num_args = 1,
+		.args = { IMM8_REG },
+		.macro = false,
+		.instruction = I_PUSH
 	},
 
 
